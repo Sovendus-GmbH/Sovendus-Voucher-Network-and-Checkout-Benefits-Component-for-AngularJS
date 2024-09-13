742be4f466836850dc33e9ac7d9e67ce718cc25c
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2024-09-13 10:59:53 +0200

    add parameter consumerDateOfBirth

--- README.md
+++ README.md
@@ -4,35 +4,37 @@
 > This component is built for the deprecated Angularjs, the component for Angular can be found [here](https://developer-hub.sovendus.com/Voucher-Network-Checkout-Benefits/Web-Integration/Angular-Component)
 
 ## Integrate into your AngularJS app
+
 1. Download the Sovendus component from [here](https://raw.githubusercontent.com/Sovendus-GmbH/Sovendus-Voucher-Network-and-Checkout-Benefits-Component-for-AngularJS/main/sovendus-banner.component.js) and add it into your AngularJS project / app folder
 2. In the component replace myAppModuleName with your AngularJS app name
 3. Import the component in your AngularJS app:
-    ```html
-    <script src="path/to/sovendus-banner.component.js"></script>
-    ```
-2. You can then use the component like with the following example:
-   
+   ```html
+   <script src="path/to/sovendus-banner.component.js"></script>
+   ```
+4. You can then use the component like with the following example:
+
    [Click here for detailed information on the parameters and which ones are required.](https://developer-hub.sovendus.com/Voucher-Network-Checkout-Benefits/Parameter)
-   
-    ```html
-    <sovendus-banner
-        traffic_source_number="YOUR_TRAFFIC_SOURCE_NUMBER"
-        traffic_medium_number="YOUR_TRAFFIC_MEDIUM_NUMBER"
-        session_id="session-1321"
-        timestamp="1707132442"
-        order_id="order-1234"
-        order_value="12.6"
-        order_currency="EUR"
-        used_coupon_code="coupon-code-from-order"
-        consumer_salutation="Mrs."
-        consumer_first_name="Test"
-        consumer_last_name="Tester"
-        consumer_email="test@123.de"
-        consumer_street="Street"
-        consumer_street_number="12"
-        consumer_city="City"
-        consumer_country="DE"
-        consumer_zipcode="45646"
-        consumer_year_of_birth="1991"
-    />
-    ```
+
+   ```html
+   <sovendus-banner
+     traffic_source_number="YOUR_TRAFFIC_SOURCE_NUMBER"
+     traffic_medium_number="YOUR_TRAFFIC_MEDIUM_NUMBER"
+     session_id="session-1321"
+     timestamp="1707132442"
+     order_id="order-1234"
+     order_value="12.6"
+     order_currency="EUR"
+     used_coupon_code="coupon-code-from-order"
+     consumer_salutation="Mrs."
+     consumer_first_name="Test"
+     consumer_last_name="Tester"
+     consumer_email="test@123.de"
+     consumer_street="Street"
+     consumer_street_number="12"
+     consumer_city="City"
+     consumer_country="DE"
+     consumer_zipcode="45646"
+     consumer_year_of_birth="1991"
+     consumer_date_of_birth="01.12.2020"
+   />
+   ```
--- sovendus-banner.component.js
+++ sovendus-banner.component.js
@@ -18,46 +18,52 @@ angular.module("myAppModuleName").component("sovendusBanner", {
     consumerCountry: "@",
     consumerZipcode: "@",
     consumerYearOfBirth: "@",
+    consumerDateOfBirth: "@",
   },
   template: `
     <div id="sovendus-integration-container-{{ $ctrl.sovDivId }}"></div>
   `,
-  controller: ['$scope', function ($scope) {
-    window.sovDivId = 1 + (window.sovDivId || 0);
-    this.sovDivId = window.sovDivId;
-    this.$onInit = function () {
-      window.sovIframes = window.sovIframes || [];
-      window.sovIframes.push({
-        trafficSourceNumber: $scope.$ctrl.trafficSourceNumber,
-        trafficMediumNumber: $scope.$ctrl.trafficMediumNumber,
-        sessionId: $scope.$ctrl.sessionId,
-        timestamp: $scope.$ctrl.timestamp,
-        orderId: $scope.$ctrl.orderId,
-        orderValue: $scope.$ctrl.orderValue,
-        orderCurrency: $scope.$ctrl.orderCurrency,
-        usedCouponCode: $scope.$ctrl.usedCouponCode,
-        iframeContainerId: "sovendus-integration-container-" + window.sovDivId,
-        integrationType: "angularjs-1.0.1",
-      });
-      window.sovConsumer = {
-        consumerSalutation: $scope.$ctrl.consumerSalutation,
-        consumerFirstName: $scope.$ctrl.consumerFirstName,
-        consumerLastName: $scope.$ctrl.consumerLastName,
-        consumerEmail: $scope.$ctrl.consumerEmail,
-        consumerStreet: $scope.$ctrl.consumerStreet,
-        consumerStreetNumber: $scope.$ctrl.consumerStreetNumber,
-        consumerCity: $scope.$ctrl.consumerCity,
-        consumerCountry: $scope.$ctrl.consumerCountry,
-        consumerZipcode: $scope.$ctrl.consumerZipcode,
-        consumerYearOfBirth: $scope.$ctrl.consumerYearOfBirth,
+  controller: [
+    "$scope",
+    function ($scope) {
+      window.sovDivId = 1 + (window.sovDivId || 0);
+      this.sovDivId = window.sovDivId;
+      this.$onInit = function () {
+        window.sovIframes = window.sovIframes || [];
+        window.sovIframes.push({
+          trafficSourceNumber: $scope.$ctrl.trafficSourceNumber,
+          trafficMediumNumber: $scope.$ctrl.trafficMediumNumber,
+          sessionId: $scope.$ctrl.sessionId,
+          timestamp: $scope.$ctrl.timestamp,
+          orderId: $scope.$ctrl.orderId,
+          orderValue: $scope.$ctrl.orderValue,
+          orderCurrency: $scope.$ctrl.orderCurrency,
+          usedCouponCode: $scope.$ctrl.usedCouponCode,
+          iframeContainerId:
+            "sovendus-integration-container-" + window.sovDivId,
+          integrationType: "angularjs-1.0.2",
+        });
+        window.sovConsumer = {
+          consumerSalutation: $scope.$ctrl.consumerSalutation,
+          consumerFirstName: $scope.$ctrl.consumerFirstName,
+          consumerLastName: $scope.$ctrl.consumerLastName,
+          consumerEmail: $scope.$ctrl.consumerEmail,
+          consumerStreet: $scope.$ctrl.consumerStreet,
+          consumerStreetNumber: $scope.$ctrl.consumerStreetNumber,
+          consumerCity: $scope.$ctrl.consumerCity,
+          consumerCountry: $scope.$ctrl.consumerCountry,
+          consumerZipcode: $scope.$ctrl.consumerZipcode,
+          consumerYearOfBirth: $scope.$ctrl.consumerYearOfBirth,
+          consumerDateOfBirth: $scope.$ctrl.consumerDateOfBirth,
+        };
+        const body = document.getElementsByTagName("body")[0];
+        const script = document.createElement("script");
+        script.async = true;
+        script.src =
+          "https://api.sovendus.com/sovabo/common/js/flexibleIframe.js";
+        script.type = "text/javascript";
+        body.appendChild(script);
       };
-      const body = document.getElementsByTagName("body")[0];
-      const script = document.createElement("script");
-      script.async = true;
-      script.src =
-        "https://api.sovendus.com/sovabo/common/js/flexibleIframe.js";
-      script.type = "text/javascript";
-      body.appendChild(script);
-    };
-  }]
+    },
+  ],
 });
